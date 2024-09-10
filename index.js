aba4bae9cd9fe6efede0880f0ade83d5aede122f
blankz <aahan3005@outlook.com>
2024-09-10 12:00:04 +0530

    v1.6

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.set("views", path.join(__dirname, 'views'))
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
